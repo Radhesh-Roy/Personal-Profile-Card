99964f3811ee565baa7eac87355d1e87f87d4ec6
Radhesh-Roy <radheshroy0011@gmail.com>
2026-06-21 18:22:50 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:personal_profile_card/profile_card/profile_card.dart';
 
 void main() {
-  runApp( MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Profile Card",
-    initialRoute: "/profile",
-    routes: {
-      "/profile": (contex)=>ProfileCard()
+  runApp( ScreenUtilInit(
+    designSize: Size(360, 690),
+    minTextAdapt: true,
+    splitScreenMode: true,
+    builder: (_ , child){
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Profile Card",
+        initialRoute: "/profile",
+        routes: {
+          "/profile": (contex)=>ProfileCard()
 
-    },
+        },
+      );
+    }
+
   ));
 }
--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomButton extends StatelessWidget {
+  final String buttonTitle;
+  final Color? backgroundColor, titleColor, borderColor;
   const CustomButton({
-    super.key,
+    super.key, required this.buttonTitle, this.backgroundColor, this.titleColor, this.borderColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 35,
-      width: 180,
+      height: 35.h,
+      width: 120.w,
       decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(5)
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(5),
+        border: Border.all(color: borderColor?? Colors.transparent)
       ),
-      child: Center(child: Text("Follow"),),
+      child: Center(child: Text("$buttonTitle", style: TextStyle(fontSize: 14, color: titleColor, fontWeight: FontWeight.w400),),),
     );
   }
 }
